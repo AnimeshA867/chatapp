b24dff9f6a5346b0157f06c25dbc8bd1e8082316
AnimeshA867 <animeshacharya867+github@gmail.com>
2024-10-04 14:14:58 +0545

    Updated Auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,66 +4,65 @@ import Google from "next-auth/providers/google";
 import { db } from "./db";
 import { fetchRedis } from "@/app/helper/redis";
 
-
-function getGoogleCredentials(){
-    const clientId=process.env.GOOGLE_CLIENT_ID;
-    const clientSecret= process.env.GOOGLE_CLIENT_SECRET;
-    if(!clientId||clientId.length===0){
-        throw new Error("Missing Google_CLIENT_ID");
-    }
-    if(!clientSecret||clientSecret.length===0){
-        throw new Error("Missing Google_CLIENT_SECRET");
-    }
-    return {
-        clientId,clientSecret
-    }
+function getGoogleCredentials() {
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+  if (!clientId || clientId.length === 0) {
+    throw new Error("Missing Google_CLIENT_ID");
+  }
+  if (!clientSecret || clientSecret.length === 0) {
+    throw new Error("Missing Google_CLIENT_SECRET");
+  }
+  return {
+    clientId,
+    clientSecret,
+  };
 }
 
-
-export const authOptions:NextAuthOptions={
-    adapter: UpstashRedisAdapter(db),
-    session:{
-        strategy:'jwt'
+export const authOptions: NextAuthOptions = {
+  adapter: UpstashRedisAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  providers: [
+    Google({
+      clientId: getGoogleCredentials().clientId,
+      clientSecret: getGoogleCredentials().clientSecret,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      const dbUserResult = (await fetchRedis("get", `user:${token.id}`)) as
+        | string
+        | null;
+      console.log(dbUserResult);
+      if (!dbUserResult) {
+        token.id = user!.id;
+        await db.sadd(`users`, user!.id);
+        return token;
+      }
+      const dbUser = JSON.parse(dbUserResult) as User;
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      };
     },
-    pages:{
-        signIn:'/login'
+    async session({ session, token }) {
+      if (token) {
+        (session.user.id = token.id),
+          (session.user.email = token.email),
+          (session.user.image = token.picture),
+          (session.user.email = token.email);
+      }
+      return session;
     },
-    providers:[
-        Google({
-            clientId:getGoogleCredentials().clientId,
-            clientSecret:getGoogleCredentials().clientSecret
-        })
-    ],
-    callbacks:{
-        async jwt ({token,user}){
-        
-            const dbUserResult = (await fetchRedis('get', `user:${token.id}`)) as 
-            |string|null
-
-
-            if(!dbUserResult){
-                token.id= user!.id;
-                return token;
-            }
-            const dbUser= JSON.parse(dbUserResult) as User;
-            return {
-                id:dbUser.id,
-                name:dbUser.name,
-                email:dbUser.email,
-                picture:dbUser.image,
-            }
-        },
-        async session ({session, token}){
-            if(token){
-                session.user.id= token.id,
-                session.user.email=token.email,
-                session.user.image=token.picture,
-                session.user.email=token.email
-            }
-            return session;
-        },
-        redirect(){
-            return '/dashboard'
-        }
-    }
-}
+    redirect() {
+      return "/dashboard";
+    },
+  },
+};
